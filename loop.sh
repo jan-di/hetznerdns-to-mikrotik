053090d4cb4c5255605281b094fe5b04b41d559c
Jan Dittrich <mail@jand.one>
2024-12-29 20:58:15 +0100

    Add healthcheck integration

--- loop.sh
+++ loop.sh
@@ -23,17 +23,39 @@ get_serial() {
 }
 
 while true; do
+
+    if [ -n "$HEALTHCHECK_START_URL" ]; then
+        curl -fsS "$HEALTHCHECK_START_URL"
+    fi
+    
+    success=1
     random_nameserver=$(echo "$HETZNER_NAMESERVERS" | tr ' ' '\n' | shuf | head -n 1)
     serial=$(get_serial "$ZONE" "$random_nameserver")
 
     if [ "$serial" != "$old_serial" ]; then
         echo "Serial of $ZONE changed from $old_serial to $serial. Trigger sync."
 
-        poetry run hdns2mikrotik sync --zones "$ZONE"
+        if ! poetry run hdns2mikrotik sync --zones "$ZONE"
+        then
+            echo "Failed to sync $ZONE"
+            success=0
+        fi
+    
 
         old_serial="$serial"
     else
         echo "Serial of $ZONE is still $serial. No need to sync."
+    fi
+
+    # check if success
+    if [ "$success" = "1" ]; then
+        if [ -n "$HEALTHCHECK_SUCCESS_URL" ]; then
+            curl -fsS "$HEALTHCHECK_SUCCESS_URL"
+        fi
+    else
+        if [ -n "$HEALTHCHECK_FAILURE_URL" ]; then
+            curl -fsS "$HEALTHCHECK_FAILURE_URL"
+        fi
     fi
 
     echo "Sleeping for $INTERVAL seconds"
